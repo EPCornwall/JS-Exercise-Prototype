f1e8e6062782504021eb011b71e51d1f96dff0d2
EPCornwall <Elizabeth.cornwall91@gmail.com>
2020-06-17 16:19:26 -0500

    completed MVP tasks

--- index.js
+++ index.js
@@ -45,13 +45,19 @@ function Person(name, age) {
   this.stomach = [];
 }
 
-Person.prototype.eat = function(food){
+Person.prototype.eat = function(edible){
   if(this.stomach.length < 10){
-    this.stomach.push(food);
+    this.stomach.push(edible);
   }
 }
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
 /*
   TASK 2
@@ -67,9 +73,21 @@ Person.prototype.eat = function(food){
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model,
+  this.milesPerGallon = milesPerGallon,
+  this.tank = 0
+  this.odometer = 0
 }
+
+Car.prototype.fill = function(gallons){
+  return this.tank += gallons;
+}
+
+// Car.prototype.drive = function(distance){
+//   this.odometer += distance;
+//   this.tank = this.tank - (distance/this.milesPerGallon);
+// }
 
 /*
   TASK 3
@@ -78,9 +96,17 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
+
 
 /* 
   TASK 4
